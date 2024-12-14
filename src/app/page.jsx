19b2ef3e7a8988f83a78179495328815ea1c0b9a
Harsh Boghara <harshboghra2004@gmail.com>
2024-12-15 00:03:01 +0530

    all in root route

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Hero from "./hero/page";
 import EnterPrise from "./enterprise/page";
+import GameChanger from "./gamechanger/page";
 
 const Home = () => {
     return (
-        <main className="bg-[#161616] w-screen h-screen overflow-auto py-8">
+        <main className="bg-[#161616]  overflow-auto py-8">
             {/* Hero Section */}
             <Hero />
 
             {/* EnterPrise Section */}
             <EnterPrise />
+
+            {/* Game Changer Section */}
+            <GameChanger />
+
         </main>
     );
 };
